f8a810e6502c2aa6a56c93636db6f28730b5f624
nvchaitanya <chathan2569@gmail.com>
2020-05-02 15:59:49 +0530

    github-user-search

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import User from './components/User'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+class App extends React.Component{
+    render(){
+        return(
+            <User />
+        )
+    }
+}
+export default App
